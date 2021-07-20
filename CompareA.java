64c21e8558ccbb4826ab53306e43e42fdfacc23b
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-07-20 13:58:08 +0800

    Comparison of the area of two circles
    This is about the interface of the entry operation, more code, but also class inheritance, rewriting interface and other operations an overview.

---
+++ CompareA.java
@@ -0,0 +1,20 @@
+package Day24;
+
+/**
+ *
+ *
+ *
+ */
+public interface CompareA {
+    //静态方法
+    public static void method1(){
+        System.out.println("CompareA:北京");
+    }
+    //默认方法
+    public default void method2(){
+        System.out.println("CompareA:上海");
+    }
+    default  void method3(){
+        System.out.println("CompareA：上海");
+    }
+}
---
+++ CompareObject.java
@@ -0,0 +1,5 @@
+package Day24;
+
+public interface CompareObject {
+    public int compareTo(Object o);
+}
